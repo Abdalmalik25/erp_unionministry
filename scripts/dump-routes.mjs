4f845c499a967f04848585a0d17c0315690a175a
dynamicgsye-prog <dynamicgsye@gmail.com>
2026-08-25 03:32:49 +0300

    chore(scripts): تنظيف أدوات الفحص المؤقتة بعد نجاح النشر

--- scripts/dump-routes.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-const s = fs.readFileSync('src/app/routes.tsx', 'utf8');
-const re = /path="([^"]+)"/g;
-let m;
-const out = [];
-while ((m = re.exec(s)) !== null) out.push(m[1]);
-console.log(out.join('\n'));
